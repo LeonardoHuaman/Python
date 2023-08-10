0e79d7e47469f6e1fd445ce1aaee83b57250418f
LeonardoHuaman <107220070+LeonardoHuaman@users.noreply.github.com>
2023-08-10 09:30:51 -0500

    Ejercicio semana 1
    Ejercicio avanzado de la semana 1 sobre funciones

---
+++ ejercicios.py
@@ -0,0 +1,33 @@
+'''
+EJERCICIO 1
+import math
+N1 = float2(input("Inserte el radio de la esfera: "))
+
+AREA = 4*math.pi*N1**2
+VOLUMEN = 4/3*math.pi*N1**3
+print("El area de la esfera es: ", round(AREA, 2))
+print("El volumen de la esfera es: ", round(VOLUMEN, 2))
+'''
+'''
+#EJERCICIO 2
+
+N1 = int(input("Ingrese la cantidad en soles: "))
+dol = N1/3.75
+eur = N1/4.12
+print("La cantidad en dolares es ", round(dol, 2))
+print("La cantidad en euros es ", round(eur,2))
+'''
+'''
+#EJERCICIO 3
+N1 = input("Inserte su nombre: ")
+N2 = int(input("Inserte su edad: "))
+
+edad = (2022 - N2)+100
+print( N1 + " Cumpliras 100 añios en el anio " + str(edad))
+'''
+'''
+#EJERCICIO 4
+N1 = int(input("Ingrese la cantidad de minutos: "))
+segundos = N1*60
+print(str(N1)+ " minutos equivale a " + str(segundos) + " segundos" )
+'''
