a7e116c585e74fd8c82e44fd75990b76088e15d3
LeonardoHuaman <107220070+LeonardoHuaman@users.noreply.github.com>
2023-08-10 09:29:20 -0500

    Nuevo archivo
    Archivo agregado de la semana 1

---
+++ helloworld.py
@@ -0,0 +1,44 @@
+'''
+
+name = input("Ingrese tu nombre: ")
+print(type(name))
+print("Tu nombre es " + name)
+
+#age = int(input("Ingresa tu edad: "))
+#print(type(age))
+#print("Tu edad es " + str(age + 2))
+#print("Tu edad es ", age + 2)
+
+
+#s = "Lionel \"La Pulga\" Messi"
+#print(s)
+#s2 = "Hola\n\tComo estas"
+#print(s2)
+
+#x = 45
+#print(45)
+#x = 0b100011
+#print(x)
+
+#b = bool(0)
+#print(b)
+#b = bool(4)
+#print(b)
+#b = bool(1.234)
+#print(b)
+
+s = "Juan"
+print(id(s))
+print (s)
+s = "Pedro"
+print(id(s))
+print (s)
+
+y = "Juan"
+print(id(y))
+print (y)
+#y[0]="M" IMPOSIBLE
+
+# + : suma -:resta *:multiplicacion /:division  //:division entera %:residuo **:potencia
+
+'''
